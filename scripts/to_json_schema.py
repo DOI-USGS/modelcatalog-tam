9838f7239f2f3b67c3979c6c0649b709036fea42
Brandon Serna <bserna@usgs.gov>
2021-03-01 13:26:00 -0700

    🎨  json schema

---
+++ scripts/to_json_schema.py
@@ -0,0 +1,7 @@
+import os 
+os.chdir('../metadata_schema')
+
+from BasicProfileSchema import BasicProfile
+
+with open('json-schema/BasicProfile.json', 'w') as f:
+    f.write(BasicProfile.schema_json(indent=2))
