21bb241d39b508def49545fb686e10510c73d476
Brandon Serna <bserna@usgs.gov>
2022-01-07 13:47:51 -0700

    🗃️  updated json schema file

--- scripts/to_json_schema.py
+++ scripts/to_json_schema.py
@@ -1,15 +1,25 @@
 '''
 Run script within the scripts directory
+
+If the python context gives errors:
+
+`cd metadata_schema` run python repl
+
+>
+>from BasicProfileSchema import BasicProfile
+>with open('json-schema/BasicProfile.json', 'w') as f:
+    f.write(BasicProfile.schema_json(indent=2))
 '''
 
 import os 
 from pathlib import Path
 
-cpath = Path.cwd().parents[0]
-npath = Path.joinpath(cpath, 'metadata_schema')
+#cpath = Path.cwd().parents[0]
+#path = Path.joinpath(cpath, 'metadata_schema')
 
-os.chdir(npath)
+#os.chdir(npath)
 
+print(Path.cwd())
 from BasicProfileSchema import BasicProfile
 
 with open('json-schema/BasicProfile.json', 'w') as f:
